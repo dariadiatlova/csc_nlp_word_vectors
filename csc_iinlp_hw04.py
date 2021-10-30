bf6c09ab97746f649d5476916b04e49776b62716
diatlova <dari.diatlova@gmail.com>
2021-10-30 16:53:37 +0300

    updated script

--- csc_iinlp_hw04.py
+++ csc_iinlp_hw04.py
@@ -38,7 +38,9 @@ log = logging.getLogger()
 
 
 class Sentences(object):
-    """ Итератор по строкам заданных файлов, не загружающий их полностью в RAM """
+    """
+        Итератор по строкам заданных файлов, не загружающий их полностью в RAM
+    """
 
     def __init__(self, filenames: Iterable[str]):
         self.filenames: Iterable = filenames
@@ -80,7 +82,9 @@ class Embeddings(object):
 
     @lru_cache(maxsize=100000000)
     def _idx(self, token: str):
-        """ Кэширующий метод, получающий индекс терма """
+        """
+            Кэширующий метод, получающий индекс терма
+        """
 
         if token not in self.token2id:
             curr_idx = len(self.id2token)
@@ -91,7 +95,9 @@ class Embeddings(object):
         return curr_idx
 
     def _reset(self):
-        """ Сброс всего 'выученного' (но не гиперпараметров) """
+        """
+            Сброс всего 'выученного' (но не гиперпараметров)
+        """
         self.M, self.counts, self.token2id, self.id2token = None, None, {}, []
 
     def fit(self, text_reader: Callable):
@@ -135,13 +141,11 @@ class Embeddings(object):
                 current_idx = self._idx(sentence[c])
                 total_counts[current_idx] += 1
 
-                # TODO: проходим for вдоль окна индексом idx и делаем, что должно
-                # TODO: ...не забывая о for_exclusion, если будем использовать
+                # итерируемся в окне, заполняем счетчики, пропускаем редкие и популярные слова
                 window = np.arange(c - self.whs, c + self.whs + 1)
                 for idx in window:
                     if idx < 0 or idx >= len(sentence) or idx == c or sentence[idx] in for_exclusion:
                         continue
-
                     values.append(1)
                     fromm.append(current_idx)
                     too.append(self._idx(sentence[idx]))
@@ -152,31 +156,31 @@ class Embeddings(object):
         self.M = self.M.tocsr()
 
         # укладываем общую встречаемость термов в массив
-        # TODO: как бы нам их использовать?
         self.counts = sp.csr_matrix(np.array([total_counts[i] for i in range(len(self.id2token))]))
 
+        # считаем априорные вероятности для всех слов в словаре
         all_word_counts = np.sum(self.counts.toarray()[0])
         probabilities = self.counts.toarray()[0] / all_word_counts
         count_matrix = self.M.toarray()
-        self.pmi_matrix = np.zeros((len(probabilities), len(probabilities)), dtype=np.float32)
 
+        # считаем совместные вероятности слов и PMI матрицу
         probability_matrix = probabilities @ probabilities.T
         probability_matrix = np.where(probability_matrix <= 0, 1, probability_matrix)
         pmi_matrix = count_matrix / probability_matrix
         pmi_matrix = np.where(pmi_matrix <= 0, 0, np.log2(pmi_matrix))
 
+        # снижаем размерность матрицы с помощью SVD – получаем матрицу эмбеддингов размерности dict_size x 64
         svd = TruncatedSVD(n_components=64, n_iter=8, random_state=8)
         self.M = svd.fit_transform(pmi_matrix)
         log.debug(f"A total of {len(self.id2token)} terms.")
         log.debug(f"Matrix of size {self.M.shape} was built.")
 
-        # nonzero_size = len(self.M.nonzero()[0])
-        # log.debug(f"Sparsity: %.2f nonzero values: a total of %d." % \
-        #           (100 * nonzero_size / self.M.shape[0] / self.M.shape[1], nonzero_size))
-        return None
+        return
 
     def score(self, word1: str, word2: str):
-        """ Косинусное расстояние между векторами заданных термов """
+        """
+            Косинусное расстояние между векторами заданных термов
+        """
 
         if self.M is None:
             raise Exception("The model has not been trained yet.")
@@ -187,7 +191,9 @@ class Embeddings(object):
         return 1 - v1.dot(v2.T) / np.sqrt(np.sum(v1 * v1)) / np.sqrt(np.sum(v2 * v2))
 
     def are_similar(self, word1: str, word2: str):
-        """ Если расстояние ниже порога, то считаем близкими """
+        """
+            Если расстояние ниже порога, то считаем близкими
+        """
         if self.M is None:
             raise Exception("The model has not been trained yet.")
 
